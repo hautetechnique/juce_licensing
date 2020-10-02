0015ef3aa02a3e72338b352d8344e63895aa78a5
Jan <jdboer@hotmail.com>
2020-10-02 11:38:31 +0200

    .gitignore

---
+++ README.md
@@ -0,0 +1,6 @@
+# juce_licensing 
+
+This module enables you to add licensing to your JUCE project.
+
+This repository is private. For inquiries, please contact [Haute Technique](mailto:sander@hautetechnique.com)
+ 
---
+++ juce_licensing.h
@@ -0,0 +1,29 @@
+
+/*
+  ==============================================================================
+
+  ==============================================================================
+
+  BEGIN_JUCE_MODULE_DECLARATION
+ 
+  ID:            juce_licensing
+  vendor:        Haute Technique
+  version:       1.0.0
+  name:          Licensing for Juce
+  description:   Added licensing to any Juce application
+
+  dependencies:  juce_core
+  website:       http://www.hautetechnique.com/
+  license:       HT-PRIVATE
+
+  END_JUCE_MODULE_DECLARATION
+ 
+  ==============================================================================
+ */
+
+#pragma once
+
+#include <juce_core/juce_core.h>
+
+#endif
+
